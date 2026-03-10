e24fb0b503438bf2e1c1b80d74a4faf481a6e628
vitalhit <vitalhit@gmail.com>
2026-03-10 20:10:42 +0300

    html

--- views/site/login.php
+++ views/site/login.php
@@ -5,13 +5,13 @@ use yii\bootstrap\ActiveForm;
 ?>
 <div> <!-- class="site-login" -->
     <div class="col-xs-12 col-sm-4 col-sm-offset-4">
-        <h2 class="mt0">Войти</h2>
+        <h2 class="mt0">&nbsp;</h2>
         <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
         <?= $form->field($model, 'username') ?>
         <?= $form->field($model, 'password')->passwordInput() ?>
         <div style="float:left">
             <?= $form->field($model, 'rememberMe')->checkbox() ?>
-            <a href="/site/request-password-reset">Забыли пароль?</a>
+            <a href="/signup">Регистрация</a> |  <a href="/site/request-password-reset">Забыли пароль?</a>
         </div>
         <div class="form-group clear">
             <?= Html::submitButton('Войти', ['class' => 'btn btn-success pull-right', 'name' => 'login-button']) ?>
